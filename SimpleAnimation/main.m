b95710e4c07e44014f1dc187a60e298b585f6a30
zangqilong <zangqilong@gmail.com>
2015-12-18 19:34:53 +0800

    Initial Commit

---
+++ SimpleAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleAnimation
+//
+//  Created by 臧其龙 on 15/12/18.
+//  Copyright © 2015年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
